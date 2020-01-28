867950b49c2513f98c85f6ef84636a8e690303ba
Milton Lima <milton.lima@outlook.com>
2020-01-27 21:43:33 -0300

    Adicionado Firebase

--- js/documentos.js
+++ js/documentos.js
@@ -4,5 +4,12 @@ function search(){
     validation(cpf);
 }
 function validation(cpfValue){
+    document.getElementById('busca').setAttribute("class", "hide")
+    document.getElementById('resultado').removeAttribute("class", "hide")
     document.getElementById('tituloDocumentos').innerHTML = 'Certificado de: '+cpfValue;
+}
+function back(){
+    document.getElementById('busca').removeAttribute("class", "hide")
+    document.getElementById('resultado').setAttribute("class", "hide")
+    document.getElementById("inputCPF").value = '';
 }
---
+++ js/init.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD0gv1FPhShiu5o-ynwfjdXlJoOr9pBPEY",
+    authDomain: "documentos-84a9d.firebaseapp.com",
+    databaseURL: "https://documentos-84a9d.firebaseio.com",
+    projectId: "documentos-84a9d",
+    storageBucket: "documentos-84a9d.appspot.com",
+    messagingSenderId: "192479888755",
+    appId: "1:192479888755:web:ac988f46b9e7d922cbf106"
+    };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
